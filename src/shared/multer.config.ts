a9d3569c61ad3ef23029daa49232aed01a3ddb6f
Hernan <hernancaparros@gmail.com>
2025-04-11 09:50:11 -0300

    Modificaciones en archivo multer por problemas para deploy en Render

--- src/shared/multer.config.ts
+++ src/shared/multer.config.ts
@@ -1,11 +1,12 @@
-import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import path from 'path';
+import { Request } from 'express';
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
     cb(null, 'uploads/');
   },
-  filename: (req, file, cb) => {
+  filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     const ext = path.extname(file.originalname);
     cb(null, `poster-${uniqueSuffix}${ext}`);
@@ -14,7 +15,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ 
   storage: storage,
-  fileFilter: (req, file, cb) => {
+  fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
     if (file.mimetype.startsWith('image/')) {
       cb(null, true);
     } else {
